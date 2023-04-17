bb19c67f8e7b28e4ded8b4649b494089398ead2e
PankajDivgi <pankaj.divgi@gmail.com>
2023-04-17 23:03:08 +0530

    refresh, card design

--- src/UserInfo.tsx
+++ src/UserInfo.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 function UserInfo() {
   const [data, setData] = useState<any>([]);
@@ -7,11 +8,10 @@ function UserInfo() {
   const fetchData = async () => {
     try {
       const response = await axios.get("https://randomuser.me/api/");
-      console.log(response.data);
       setData(response.data.results[0]);
       localStorage.setItem(
-        "name",
-        JSON.stringify(response.data.results[0].name)
+        "UserInfo",
+        JSON.stringify(response.data.results[0])
       );
     } catch (error) {
       console.log(error);
@@ -23,13 +23,15 @@ function UserInfo() {
   }, []);
 
   return (
-    <div>
-      <h1>Random User:</h1>
-      <p>
-        Name: {data?.name?.title} {data?.name?.first} {data?.name?.last}
-      </p>
-      <p>Email: {data.email}</p>
-      <button onClick={()=>setData(data)}>Refresh</button>
+    <div className="card">
+      <div>
+        <h1>Random User</h1>
+        <p>
+          Name: {data?.name?.title} {data?.name?.first} {data?.name?.last}
+        </p>
+        <p>Email: {data.email}</p>
+        <button onClick={() => fetchData()}>Refresh</button>
+      </div>
     </div>
   );
 }
